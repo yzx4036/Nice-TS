bf09ca5a5bc0909162e48280c491907029f4aaa5
E_Ye <E_Ye@outlook.com>
2021-01-25 08:21:29 +0800

    feature/升级unity2020

--- Assets/Puerts/Src/Editor/CJSImporter.cs
+++ Assets/Puerts/Src/Editor/CJSImporter.cs
@@ -1,12 +1,12 @@
 #if UNITY_2018_1_OR_NEWER
 using System.IO;
-using UnityEditor.Experimental.AssetImporters;
+
 using UnityEngine;
  
-[ScriptedImporter(1, "cjs")]
-public class CJSImporter : ScriptedImporter
+[UnityEditor.AssetImporters.ScriptedImporter(1, "cjs")]
+public class CJSImporter : UnityEditor.AssetImporters.ScriptedImporter
 {
-    public override void OnImportAsset(AssetImportContext ctx)
+    public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext ctx)
     {
         TextAsset subAsset = new TextAsset(File.ReadAllText(ctx.assetPath));
         ctx.AddObjectToAsset("text", subAsset);
